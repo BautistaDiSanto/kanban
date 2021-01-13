dcf4dbd2c1e1ce13a9a0a0d0d4bae8f8634fb180
BautistaDiSanto <juanbautistadisanto@gmail.com>
2021-01-13 16:17:27 -0300

    global css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { TaskModal } from "./components/taskModal/taskModal";
+import { Column } from "./components/column/column";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "black",
+        height: "100vh",
+      }}
+    >
+      <Column />
+      <Column />
+      <Column />
+      <TaskModal />
+    </div>
+  );
 }
 
 export default App;
